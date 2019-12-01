f5610b598cd5c11f89613d06e965d771f323db3e
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-12-01 22:15:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,5 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do 
     enable :sessions
-    set :sessions_secret ""
+    set :sessions_secret 
 end
+ get '/'do 
+   erb:index
+ end 
+ 
